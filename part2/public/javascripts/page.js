fced0e31b0b57efcbf13d416749d55dff07548c7
shauncsy <19710513195@163.com>
2025-06-20 15:06:53 +0000

    [automatic commit] 2025-06-20 15:06:53 UTC 180.150.112.105

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -1,75 +1,70 @@
-var posts = [];
-var search = null;
+var posts = []
+var search = null
 
 /*
  * Hides the main part of the page to show the Ask a Question section
  */
-function showAsk(){
-    var main = document.getElementById("main");
-    var ask = document.getElementById("ask");
-    main.style.display = "none";
-    ask.style.display = "block";
+function showAsk() {
+  var main = document.getElementById('main')
+  var ask = document.getElementById('ask')
+  main.style.display = 'none'
+  ask.style.display = 'block'
 }
 
 /*
  * Hides the Ask a Question section of the page to show the main part,
  * clearing the question input fields.
  */
-function showMain(){
-    var main = document.getElementById("main");
-    var ask = document.getElementById("ask");
-    ask.style.display = "none";
-    main.style.display = "block";
+function showMain() {
+  var main = document.getElementById('main')
+  var ask = document.getElementById('ask')
+  ask.style.display = 'none'
+  main.style.display = 'block'
 
-    document.getElementById('post-title').value = '';
-    document.getElementById('post-content').value = '';
-    document.getElementById('post-tags').value = '';
+  document.getElementById('post-title').value = ''
+  document.getElementById('post-content').value = ''
+  document.getElementById('post-tags').value = ''
 }
 
 /*
  * Creates a new question/post & send it to the server, before triggering an update for the main part of the page.
  */
-function createPost(){
+function createPost() {
+  search = null
 
-    search = null;
+  let post = {
+    title: document.getElementById('post-title').value,
+    content: document.getElementById('post-content').value,
+    tags: document.getElementById('post-tags').value.split(' '),
+    upvotes: 0,
+  }
 
-    let post = {
-        title: document.getElementById('post-title').value,
-        content: document.getElementById('post-content').value,
-        tags: document.getElementById('post-tags').value.split(" "),
-        upvotes: 0
-    };
+  // Create AJAX Request
+  var xmlhttp = new XMLHttpRequest()
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
+  // Define function to run on response
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Update the page on success
+      loadPosts()
+      showMain()
+    }
+  }
 
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // Update the page on success
-            loadPosts();
-            showMain();
-        }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/addpost", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(post));
-
+  // Open connection to server & send the post data using a POST request
+  // We will cover POST requests in more detail in week 8
+  xmlhttp.open('POST', '/addpost', true)
+  xmlhttp.setRequestHeader('Content-type', 'application/json')
+  xmlhttp.send(JSON.stringify(post))
 }
 
 /*
  * Updates the search term then reloads the posts shown
  */
-function searchPosts(){
-
-    search = document.getElementById('post-search').value.toUpperCase();
-    updatePosts();
-
+function searchPosts() {
+  search = document.getElementById('post-search').value.toUpperCase()
+  updatePosts()
 }
-
 
 /*
  * Reloads the posts shown on the page
@@ -77,35 +72,35 @@ function searchPosts(){
  * If a search term is being used
  */
 function updatePosts() {
+  // Reset the page
+  document.getElementById('post-list').innerHTML = ''
 
-    // Reset the page
-    document.getElementById('post-list').innerHTML = '';
+  // Iterate over each post in the array by index
+  for (let i = 0; i < posts.length; i++) {
+    let post = posts[i]
 
-    // Iterate over each post in the array by index
-    for(let i=0; i<posts.length; i++){
+    // Check if a search term used.
+    if (search !== null) {
+      // If so, skip this question/post if title or content doesn't match
+      if (
+        post.title.toUpperCase().indexOf(search) < 0 &&
+        post.content.toUpperCase().indexOf(search) < 0
+      ) {
+        continue
+      }
+    }
 
-        let post = posts[i];
+    // Generate a set of spans for each of the tags
+    let tagSpans = ''
+    for (let tag of post.tags) {
+      tagSpans = tagSpans + `<span class="tag">${tag}</span>`
+    }
 
-        // Check if a search term used.
-        if(search !== null){
-            // If so, skip this question/post if title or content doesn't match
-            if (post.title.toUpperCase().indexOf(search) < 0 &&
-                post.content.toUpperCase().indexOf(search) < 0 ) {
-                continue;
-            }
-        }
+    // Generate the post/question element and populate its inner HTML
+    let postDiv = document.createElement('DIV')
+    postDiv.classList.add('post')
 
-        // Generate a set of spans for each of the tags
-        let tagSpans = '';
-        for(let tag of post.tags){
-            tagSpans = tagSpans + `<span class="tag">${tag}</span>`;
-        }
-
-        // Generate the post/question element and populate its inner HTML
-        let postDiv = document.createElement("DIV");
-        postDiv.classList.add("post");
-
-        postDiv.innerHTML = `
+    postDiv.innerHTML = `
             <div class="votes">
                 <button onclick="upvote(${i})">+</button>
                 <p><span class="count">${post.upvotes}</span><br />votes</p>
@@ -115,16 +110,15 @@ function updatePosts() {
                 <h3><a href="#">${post.title}</a></h3>
                 <i>By ${post.author}</i>
                 <p>${post.content}</p>
-                ${tagSpans}<span class="date">${new Date(post.timestamp).toLocaleString()}</span>
+                ${tagSpans}<span class="date">${new Date(
+      post.timestamp
+    ).toLocaleString()}</span>
             </div>
-        `;
+        `
 
-        // Append the question/post to the page
-        document.getElementById("post-list").appendChild(postDiv);
-
-    }
-
-
+    // Append the question/post to the page
+    document.getElementById('post-list').appendChild(postDiv)
+  }
 }
 
 /*
@@ -134,80 +128,81 @@ function updatePosts() {
  * - Update the
  */
 function loadPosts() {
+  // Create AJAX Request
+  var xmlhttp = new XMLHttpRequest()
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
+  // Define function to run on response
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Parse the JSON and update the posts array
+      posts = JSON.parse(this.responseText)
+      // Call the updatePosts function to update the page
+      updatePosts()
+    }
+  }
 
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // Parse the JSON and update the posts array
-            posts = JSON.parse(this.responseText);
-            // Call the updatePosts function to update the page
-            updatePosts();
-        }
-    };
+  // Open connection to server
+  xmlhttp.open('GET', '/posts', true)
 
-    // Open connection to server
-    xmlhttp.open("GET", "/posts", true);
-
-    // Send request
-    xmlhttp.send();
-
+  // Send request
+  xmlhttp.send()
 }
-
 
 /*
  * Increase the votes for a given post, then update the page
  */
 function upvote(index) {
-    posts[index].upvotes ++;
-    updatePosts();
+  posts[index].upvotes++
+  updatePosts()
 }
 
 /*
  * Decrease the votes for a given post, then update the page
  */
 function downvote(index) {
-    posts[index].upvotes --;
-    updatePosts();
+  posts[index].upvotes--
+  updatePosts()
 }
 
+function login() {
+  let user = {
+    username: document.getElementById('username').value,
+    password: document.getElementById('password').value,
+  }
 
-function login(){
+  // Create AJAX Request
+  var xmlhttp = new XMLHttpRequest()
 
-    let user = {
-        user: document.getElementById('username').value,
-        pass: document.getElementById('password').value
-    };
+  // Define function to run on response
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const response = JSON.parse(this.responseText)
+      const user = response.user
+      if (user.role === 'owner') {
+        window.location.href = 'owner-dashboard.html'
+      } else {
+        window.location.href = 'walker-dashboard.html'
+      }
+    } else if (this.readyState == 4 && this.status >= 400) {
+      alert('Login failed')
+    }
+  }
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
-
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
-        }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
-
+  // Open connection to server & send the post data using a POST request
+  // We will cover POST requests in more detail in week 8
+  xmlhttp.open('POST', '/api/users/login', true)
+  xmlhttp.setRequestHeader('Content-type', 'application/json')
+  xmlhttp.send(JSON.stringify(user))
 }
 
-function logout(){
+function logout() {
+  // Create AJAX Request
+  var xmlhttp = new XMLHttpRequest()
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
+  // Open connection to server & send the post data using a POST request
+  xmlhttp.open('POST', '/api/users/logout', true)
+  xmlhttp.send()
 
-    // Open connection to server & send the post data using a POST request
-    xmlhttp.open("POST", "/users/logout", true);
-    xmlhttp.send();
-
+  // redirect to login
+  window.location.href = 'index.html'
 }
