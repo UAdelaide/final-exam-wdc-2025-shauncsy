1cf69c150f2dc3c217b61832913fb28169408611
shauncsy <19710513195@163.com>
2025-06-20 13:41:51 +0000

    [automatic commit] 2025-06-20 13:41:51 UTC 180.150.112.105

--- part1/app.js
+++ part1/app.js
@@ -141,7 +141,7 @@ let db
 
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
-        SELECT WalkApplications.request_id, WalkApplications.walker_id, Dogs.owner_id, 5, 'Too great!' FROM Walkapplications LEFT JOIN Walkrequests ON Walkapplications.request_id = Walkrequests.request_id LEFT JOIN Dogs ON Walkrequests.dog_id = Dogs.dog_id WHERE applied_at = '2025-06-09 10:00:00'
+        SELECT WalkApplications.request_id, WalkApplications.walker_id, Dogs.owner_id, 5, 'Too great!' FROM WalkApplications LEFT JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id LEFT JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id WHERE applied_at = '2025-06-09 10:00:00'
 
         `)
 
