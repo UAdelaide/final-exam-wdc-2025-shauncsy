b7501aa3ec02daa5a7cc231b7f3c9388cf8abed5
shauncsy <19710513195@163.com>
2025-06-20 12:56:42 +0000

    [automatic commit] 2025-06-20 12:56:42 UTC 180.150.112.105

--- part2/app.js
+++ part2/app.js
@@ -1,19 +1,34 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
 
-const app = express();
+const app = express()
+
+// Session
+const session = require('express-session')
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+)
 
 // Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/public')))
 
 // Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+const walkRoutes = require('./routes/walkRoutes')
+const userRoutes = require('./routes/userRoutes')
+const dogRoutes = require('./routes/dogRoutes')
+app.use('/api/walks', walkRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/dogs', dogRoutes)
 
 // Export the app instead of listening here
-module.exports = app;
+module.exports = app
